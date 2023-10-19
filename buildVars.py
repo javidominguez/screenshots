eb7b0d155d26cedf9818560881496d327e8d11a6
Javi Domínguez <fjavids@gmail.com>
2023-10-19 10:47:36 +0200

    Updated addon template

--- buildVars.py
+++ buildVars.py
@@ -25,21 +25,27 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Provides an wizard to take screenshots."""),
 	# version
-	"addon_version": "2.1.1",
+	"addon_version": "2023.1.0",
 	# Author(s)
 	"addon_author": "Javi Domínguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url": "https://github.com/javidominguez/screenshots",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/javidominguez/screenshots",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2021.2.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.1.0",
+	"addon_lastTestedNVDAVersion": "2023.3.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL 2",
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
 
 # Define the python files that are the sources of your add-on.
