3dd2558891a692acb56e2b6a16cf824e8e1556bd
Javi Domínguez <fjavids@gmail.com>
2024-03-23 22:06:32 +0100

    Release 2024.1.0 stable version

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Provides an wizard to take screenshots."""),
 	# version
-	"addon_version": "2024.0.3",
+	"addon_version": "2024.1.0",
 	# Author(s)
 	"addon_author": "Javi Domínguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
@@ -41,7 +41,7 @@ addon_info = {
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": "dev",
+	"addon_updateChannel": None,
 	# Add-on license such as GPL 2
 	"addon_license": "GPL 2",
 	# URL for the license document the ad-on is licensed under
--- changelog.md
+++
@@ -1,2 +0,0 @@
-* Updated Ukrainian translation (interface and documentation). Thanks @VovaMobile  
-* Added translations for Traditional Chinese. Thanks  @surfer0627 & @huanlin
